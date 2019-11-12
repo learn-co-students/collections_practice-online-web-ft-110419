4d933bbdb7a4440d7dd8fec3a155276a1b906f75
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-12 02:06:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort.reverse
 
+end
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+
+end
+def swap_elements(array)
+  array.insert(1,array.delete_at(2))
+end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  nd_array =array.split("")
+  nd_array.each_with_index do |x| nd_array[2] = "$" end
+end
